a705690b79e681009d50dd4cc3f6a4d213726521
adiabat <rx@awsomnet.org>
2018-02-07 16:18:46 -0500

    add comments about encoding / endianness of provided signatures

--- pset01/forge.go
+++ pset01/forge.go
@@ -2,6 +2,61 @@ package main
 
 import "fmt"
 
+/*
+A note about the provided keys and signatures:
+the provided pubkey and signature, as well as "HexTo___" functions may not work
+with all the different implementations people could built.  Specifically, they
+are tied to an endian-ness.  If, for example, you decided to encode your public
+keys as (according to the diagram in the slides) up to down, then left to right:
+<bit 0, row 0> <bit 0, row 1> <bit 1, row 0> <bit 1, row 1> ...
+
+then it won't work with the public key provided here, because it was encoded as
+<bit 0, row 0> <bit 1, row 0> <bit 2, row 0> ... <bit 255, row 0> <bit 0, row 1> ...
+(left to right, then up to down)
+
+so while in class I said that any decisions like this would work as long as they
+were consistent... that's not actually the case!  Because your functions will
+need to use the same ordering as the ones I wrote in order to create the signatures
+here.  I used what I thought was the most straightforward / simplest encoding, but
+endian-ness is something of a tabs-vs-spaces thing that people like to argue
+about :).
+
+So for clarity, and since it's not that obvious from the HexTo___ decoding
+functions, here's the order used:
+
+secret keys and public keys:
+all 256 elements of row 0, most significant bit to least significant bit
+(big endian) followed by all 256 elements of row 1.  Total of 512 blocks
+of 32 bytes each, for 16384 bytes.
+For an efficient check of a bit within a [32]byte array using this ordering,
+you can use:
+    arr[i/8]>>(7-(i%8)))&0x01
+where arr[] is the byte array, and i is the bit number; i=0 is left-most, and
+i=255 is right-most.  The above statement will return a 1 or a 0 depending on
+what's at that bit location.
+
+Messages: messages are encoded the same way the sha256 function outputs, so
+nothing to choose there.
+
+Signatures: Signatures are also read left to right, MSB to LSB, with 256 blocks
+of 32 bytes each, for a total of 8192 bytes.  There is no indication of whether
+the provided preimage is from the 0-row or the 1-row; the accompanying message
+hash can be used instead, or both can be tried.  This again interprets the message
+hash in big-endian format, where
+    message[i/8]>>(7-(i%8)))&0x01
+can be used to determine which preimage block to reveal, where message[] is the
+message to be signed, and i is the sequence of bits in the message, and blocks
+in the signature.
+
+Hopefully people don't have trouble with different encoding schemes.  If you
+really want to use your own method which you find easier to work with or more
+intuitive, that's OK!  You will need to re-encode the key and signatures provided
+in signatures.go to match your ordering so that they are valid signatures with
+your system.  This is probably more work though and I recommend using the big
+endian encoding described here.
+
+*/
+
 // Forge is the forgery function, to be filled in and completed.  This is a trickier
 // part of the assignment which will require the computer to do a bit of work.
 // It's possible for a single core or single thread to complete this in a reasonable
