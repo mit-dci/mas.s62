98fd53b547b9e25d2c682697f62261fc2f1ac012
adiabat <rx@awsomnet.org>
2018-02-07 02:18:24 -0500

    add comments and text to readme

--- pset01/forge.go
+++ pset01/forge.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
-// code for forging signatures to fill in
-
-// Geordi
-func Forge() {
+// Forge is the forgery function, to be filled in and completed.  This is a trickier
+// part of the assignment which will require the computer to do a bit of work.
+// It's possible for a single core or single thread to complete this in a reasonable
+// amount of time, but may be worthwhile to write multithreaded code to take
+// advantage of multi-core CPUs.  For programmers familiar with multithreaded code
+// in golang, the time spent on parallelizing this code will be more than offset by
+// the CPU time speedup.  For programmers with access to 2-core or below CPUs, or
+// who are less familiar with multithreaded code, the time taken in programming may
+// exceed the CPU time saved.  Still, it's all about learning.
+// The Forge() function doesn't take any inputs; the inputs are all hard-coded into
+// the function which is a little ugly but works OK in this assigment.
+// The input public key and signatures are provided in the "signatures.go" file and
+// the code to convert those into the appropriate data structures is filled in
+// already.
+// Your job is to have this function return two things: A string containing the
+// substring "forge" as well as your name or email-address, and a valid signature
+// on the hash of that ascii string message, from the pubkey provided in the
+// signatures.go file.
+// The Forge function is tested by TestForgery() in forge_test.go, so if you
+// run "go test" and everything passes, you should be all set.
+func Forge() (string, Signature, error) {
 	// decode pubkey, all 4 signatures into usable structures from hex strings
 	pub, err := HexToPubkey(hexPubkey1)
 	if err != nil {
@@ -29,21 +46,35 @@ func Forge() {
 		panic(err)
 	}
 
-	var marr []Message
+	var sigslice []Signature
+	sigslice = append(sigslice, sig1)
+	sigslice = append(sigslice, sig2)
+	sigslice = append(sigslice, sig3)
+	sigslice = append(sigslice, sig4)
 
-	marr = append(marr, GetMessageFromString("1"))
-	marr = append(marr, GetMessageFromString("2"))
-	marr = append(marr, GetMessageFromString("3"))
-	marr = append(marr, GetMessageFromString("4"))
+	var msgslice []Message
 
-	fmt.Printf("ok 1: %v\n", Verify(marr[0], pub, sig1))
-	fmt.Printf("ok 2: %v\n", Verify(marr[1], pub, sig2))
-	fmt.Printf("ok 3: %v\n", Verify(marr[2], pub, sig3))
-	fmt.Printf("ok 4: %v\n", Verify(marr[3], pub, sig4))
+	msgslice = append(msgslice, GetMessageFromString("1"))
+	msgslice = append(msgslice, GetMessageFromString("2"))
+	msgslice = append(msgslice, GetMessageFromString("3"))
+	msgslice = append(msgslice, GetMessageFromString("4"))
+
+	fmt.Printf("ok 1: %v\n", Verify(msgslice[0], pub, sig1))
+	fmt.Printf("ok 2: %v\n", Verify(msgslice[1], pub, sig2))
+	fmt.Printf("ok 3: %v\n", Verify(msgslice[2], pub, sig3))
+	fmt.Printf("ok 4: %v\n", Verify(msgslice[3], pub, sig4))
+
+	msgString := "my forged message"
+	var sig Signature
 
 	// your code here!
 	// ==
-
+	// Geordi La
 	// ==
 
+	return msgString, sig, nil
+
 }
+
+// hint:
+// arr[i/8]>>(7-(i%8)))&0x01
