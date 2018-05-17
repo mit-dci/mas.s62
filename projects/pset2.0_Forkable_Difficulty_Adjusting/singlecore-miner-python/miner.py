a9d348763da910e667057544e20294984fcc843e
Avery Lamp <averylamp@gmail.com>
2018-05-16 23:19:18 -0400

    New difficulty function for singlecore miner

--- projects/pset2.0_Forkable_Difficulty_Adjusting/singlecore-miner-python/miner.py
+++ projects/pset2.0_Forkable_Difficulty_Adjusting/singlecore-miner-python/miner.py
@@ -14,20 +14,27 @@ while True:
     while True:
         hashStr = "{} {} {}".format(blockHash, name, "{}".format(i))
         block_hash = hashlib.sha256(hashStr.encode("utf-8")).hexdigest()
-        work = 0
-        for c in block_hash:
-            if c <= '0':
-                work += 4
-            elif c <= '1':
-                work += 3
-                break
-            elif c <= '3':
-                work += 2
-                break
-            elif c <= '7':
-                work += 1
-                break
-            else:
+        work = 0.0
+        block_hash_value = int(block_hash, 16)
+        first_one = True
+        first_one_count = 0
+        print(block_hash)
+        print(block_hash_value)
+        for i in range(len(squares)):
+            if block_hash_value - squares[i] < 0: # Case bit is a 0
+                if first_one:
+                    work += 1
+                else:
+                    work += 1 / squares[255 - first_one_count]
+                    first_one_count += 1
+            else: # Case bit is 1
+                block_hash_value =  block_hash_value - squares[i]
+                if first_one:
+                    first_one = False
+                    first_one_count += 1
+                else:
+                    first_one_count += 1
+            if first_one_count >= 10:
                 break
         if work >= targetWork:
             r = requests.get("http://localhost:5000/addblock/{}/{}/{}".format(blockHash, name, "{}".format(i)))
